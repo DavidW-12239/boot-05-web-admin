437012f5db2d14c17fad93ccfa0cb6b6b8d9e17d
DavidW-12239 <davidw12239@gmail.com>
2022-05-15 03:15:55 -0700

    Initial commit

---
+++ src/main/java/com/example/admin/interceptor/LoginInterceptor.java
@@ -0,0 +1,46 @@
+package com.example.admin.interceptor;
+
+import com.example.admin.bean.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+
+/**
+ * 登陆检查
+ * 1.配置拦截器拦截哪些请求
+ * 2.把配置放在容器中
+ */
+@Controller
+public class LoginInterceptor implements HandlerInterceptor {
+    //目标方法执行之前
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //登陆检查逻辑
+        HttpSession session = request.getSession();
+        Object loginUser = session.getAttribute("loginUser");
+        if (loginUser!=null){
+            //放行
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    //目标方法执行之后
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    //页面渲染以后
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
